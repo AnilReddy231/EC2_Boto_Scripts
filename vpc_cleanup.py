1b04b3742bfad43136ab8870e1f56b1587817201
AnilKumar <45019173+AnilReddy231@users.noreply.github.com>
2019-09-03 23:05:05 -0400

    Update vpc_cleanup.py

--- vpc_cleanup.py
+++ vpc_cleanup.py
@@ -4,7 +4,7 @@ import argparse
 import logging
 from botocore.exceptions import ClientError
 from time import sleep
-
+import pdb
 
 def arg_parse(*args, **kwargs):
     parser = argparse.ArgumentParser(
@@ -48,12 +48,11 @@ def vpc_cleanup(vpcid, region):
     :param region: region name where VPC is existing
     """
     logging.info(f'Removing VPC __{vpcid}__ from AWS')
-    ec2_resource = boto3.resource('ec2', region_name=region, verify=False)
+    ec2_resource = boto3.resource('ec2', region_name=region)
     ec2_client = ec2_resource.meta.client
     vpc = ec2_resource.Vpc(vpcid)
-
+    #pdb.set_trace()
     try:
-        vpc.state
         for subnet in vpc.subnets.all():
             for instance in subnet.instances.all():
                 try:
@@ -85,10 +84,17 @@ def vpc_cleanup(vpcid, region):
             except ClientError as e:
                 logging.error(e.response['Error']['Message'])
 
-        for vpcpeer in ec2_client.describe_vpc_peering_connections()['VpcPeeringConnections']:
+        for requested_peer in vpc.requested_vpc_peering_connections.all():
             try:
-                logging.info(f"Deleting VPC Peering Connection __{vpcpeer['VpcPeeringConnectionId']}__ associated with the vpc __{vpc.id}__")
-                ec2_resource.VpcPeeringConnection(vpcpeer['VpcPeeringConnectionId']).delete()
+                logging.info(f"Deleting VPC Peering Connection __{requested_peer.id}__ associated with the vpc __{vpc.id}__")
+                requested_peer.delete()
+            except ClientError as e:
+                logging.error(e.response['Error']['Message'])
+
+        for accepted_peer in vpc.accepted_vpc_peering_connections.all():
+            try:
+                logging.info(f"Deleting VPC Peering Connection __{accepted_peer.id}__ associated with the vpc __{vpc.id}__")
+                accepted_peer.delete()
             except ClientError as e:
                 logging.error(e.response['Error']['Message'])
 
@@ -98,47 +104,50 @@ def vpc_cleanup(vpcid, region):
                     logging.info(f"Deleting the Security Group __{sg.id}__ associated with the vpc __{vpc.id}__")
                     sg.delete()
                 except ClientError as e:
-                    logging.error (e.response['Error']['Message'])
+                    logging.error(e.response['Error']['Message'])
 
-        route_tables = ec2_client.describe_route_tables ()['RouteTables']
+        route_tables = vpc.route_tables.all()
         for route_table in route_tables:
-            for route in route_table['Routes']:
-                if route['Origin'] == 'CreateRoute':
+            for route in route_table.routes:
+                if route.origin == 'CreateRoute':
                     try:
-                        logging.info(f"Delete the routes from __{route_table['RouteTableId']}__ of vpc __{vpc.id}__")
-                        ec2_client.delete_route(RouteTableId=route_table['RouteTableId'], DestinationCidrBlock=route['DestinationCidrBlock'])
+                        logging.info(f"Deleting the routes from __{route_table.id}__ of vpc __{vpc.id}__")
+                        print(route)
+                        route.delete()
                     except ClientError as e:
-                        logging.error (e.response['Error']['Message'])
-            for association in route_table['Associations']:
-                if not association['Main']:
+                        logging.error(e.response['Error']['Message'])
+            for association in route_table.associations:
+                if not association.main:
                     try:
-                        logging.info(f"Disassociating and Deleting the non-main route table __{route_table['RouteTableId']}__ of the vpc __{vpc.id}__")
-                        ec2_client.disassociate_route_table(AssociationId=association['RouteTableAssociationId'])
-                        ec2_client.delete_route_table(RouteTableId=route_table['RouteTableId'])
+                        logging.info(f"Disassociating and Deleting the non-main route table __{route_table.id}__ of the vpc __{vpc.id}__")
+                        association.delete()
                     except ClientError as e:
-                        logging.error (e.response['Error']['Message'])
+                        logging.error(e.response['Error']['Message'])
         for route_table in route_tables:
-            if not route_table['Associations']:  # If Empty
+            if not route_table.associations:  # If Empty
                 try:
-                    logging.info(f"Deleting the route table __{route_table['RouteTableId']}__ of vpc __{vpc.id}__")
-                    ec2_client.delete_route_table(RouteTableId=route_table['RouteTableId'])
+                    logging.info(f"Deleting the route table __{route_table.id}__ of vpc __{vpc.id}__")
+                    route_table.delete()
                 except ClientError as e:
-                    logging.error (e.response['Error']['Message'])
+                    logging.error(e.response['Error']['Message'])
 
-        for endpoint in ec2_client.describe_vpc_endpoints()['VpcEndpoints']:
+        for endpoint in ec2_client.describe_vpc_endpoints(Filters=[{
+                    'Name': 'vpc-id',
+                    'Values': [vpcid]
+                }])['VpcEndpoints']:
             try:
                 logging.info(f"Deleting the VPC Endpoint __{endpoint['VpcEndpointId']}__ of the vpc __{vpc.id}__")
                 ec2_client.delete_vpc_endpoints(VpcEndpointIds=[endpoint['VpcEndpointId']])
             except ClientError as e:
-                logging.error (e.response['Error']['Message'])
+                logging.error(e.response['Error']['Message'])
 
         try:
             logging.info(f"All the Dependencies of the VPC __{vpc.id}__ are being removed. Proceeding on to Deleting the VPC")
             ec2_client.delete_vpc(VpcId=vpc.id)
         except ClientError as e:
-            logging.error (e.response['Error']['Message'])
+            logging.error(e.response['Error']['Message'])
     except ClientError as e:
-        logging.error (e.response['Error']['Message'])
+        logging.error(e.response['Error']['Message'])
 
 
 if __name__ == '__main__':
